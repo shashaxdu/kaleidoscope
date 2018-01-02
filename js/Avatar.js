1a195ebd90f4c12a5be35de70f4247f4deabfa3a
robin <robin.kath.wilson@gmail.com>
2018-01-01 21:09:43 -0800

    removed unnecessary navbar javascript

--- js/Avatar.js
+++ js/Avatar.js
@@ -1,56 +1,6 @@
 // JavaScript Document
 $(document).ready(function(){
 
-//NAVBAR LEFT
-   
-// $("#Me_Nav_Icon").hover(function(){
-//         $("#Me_Nav_Icon").animate({width:"45px"}, 400);
-
-//         }, function(){
-//         $("#Me_Nav_Icon").animate({width:"50px"}, 400);
-//         }
-//     );
-
-     $("#Earth_Nav_Icon").hover(function(){
-        $("#Earth_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#Earth_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-      $("#SolarSystem_Nav_Icon").hover(function(){
-        $("#SolarSystem_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#SolarSystem_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-       $("#MilkyWay_Nav_Icon").hover(function(){
-        $("#MilkyWay_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#MilkyWay_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-        $("#ObUniv_Nav_Icon").hover(function(){
-        $("#ObUniv_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#ObUniv_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-         $("#Infinity_Nav_Icon").hover(function(){
-        $("#Infinity_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#Infinity_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
 	//when BuildAvatar is clicked
     $(".buildavatar").click(function(){
     	// remove click function so can't be clicked again
--- js/Golden_Record.js
+++ js/Golden_Record.js
@@ -22,60 +22,6 @@ $(document).ready(function(){
     }
     );
 
-    
-
-   
-    $("#Me_Nav_Icon").hover(function(){
-        $("#Me_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#Me_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-     $("#Earth_Nav_Icon").hover(function(){
-        $("#Earth_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#Earth_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-    //   $("#SolarSystem_Nav_Icon").hover(function(){
-    //     $("#SolarSystem_Nav_Icon").animate({width:"45px"}, 400);
-
-    //     }, function(){
-    //     $("#SolarSystem_Nav_Icon").animate({width:"50px"}, 400);
-    //     }
-    // );
-
-       $("#MilkyWay_Nav_Icon").hover(function(){
-        $("#MilkyWay_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#MilkyWay_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-        $("#ObUniv_Nav_Icon").hover(function(){
-        $("#ObUniv_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#ObUniv_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-         $("#Infinity_Nav_Icon").hover(function(){
-        $("#Infinity_Nav_Icon").animate({width:"45px"}, 400);
-
-        }, function(){
-        $("#Infinity_Nav_Icon").animate({width:"50px"}, 400);
-        }
-    );
-
-
-
-
     $("#voyagerimg").hover(function(){
 
         $("#earthtext").css("visibility", "hidden");
